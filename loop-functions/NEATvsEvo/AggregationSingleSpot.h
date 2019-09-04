1b5370f874bb2a41c6a88c393fae36a76333132f
Ken <ken@kenh.fr>
2019-09-04 23:30:37 +0200

    new aggreg simple

---
+++ loop-functions/NEATvsEvo/AggregationSingleSpot.h
@@ -0,0 +1,51 @@
+/**
+  *
+  * @author Antoine Ligot - <aligot@ulb.ac.be>
+  *
+  * @package ARGoS3-AutoMoDe
+  *
+  * @license MIT License
+  */
+
+#ifndef AGGREGATION_SINGLE_SPOT
+#define AGGREGATION_SINGLE_SPOT
+
+#include <argos3/core/simulator/space/space.h>
+#include <argos3/plugins/robots/e-puck/simulator/epuck_entity.h>
+
+#include "../../src/CoreLoopFunctions.h"
+#include "/home/aligot/Desktop/Arena/NEAT-private/src/controllers/NeuralNetworkRM1Dot1Binary.h"
+
+using namespace argos;
+
+class AggregationSingleSpot: public CoreLoopFunctions {
+  public:
+    AggregationSingleSpot();
+    AggregationSingleSpot(const AggregationSingleSpot& orig);
+    virtual ~AggregationSingleSpot();
+
+    virtual void Destroy();
+    virtual void Init(TConfigurationNode& t_tree);
+
+    virtual argos::CColor GetFloorColor(const argos::CVector2& c_position_on_plane);
+    virtual void PostExperiment();
+    virtual void PostStep();
+    virtual void Reset();
+
+    Real GetObjectiveFunction();
+
+    CVector3 GetRandomPosition();
+
+  private:
+    Real m_fRadius;
+    CVector2 m_cCoordSpot1;
+    UInt32 m_unScoreSpot1;
+    Real m_fObjectiveFunction;
+    Real m_fHeightNest;
+
+    std::string m_strOutputFile;
+    std::ofstream m_cTrace;
+    std::ofstream m_cFitness;
+};
+
+#endif
